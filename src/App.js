76501aac0f7164d390393f6eed4ea5ad51f84154
chetin4ever <hetan009@gmail.com>
2020-07-27 14:24:38 +0530

    adding the bootstrap and adding navbar and card for contact

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Contact from './component/Contact';
 import Header from './component/Header';
@@ -7,10 +7,13 @@ import Header from './component/Header';
 function App() {
   return (
     <div className="App">
-      <Header branding="Conatct Manager"/>
-      <Contact name="john D" email="jhonDoe@gmail.com" phone="555-555-5555" />
-      <Contact name="abc" email="abc@gmail.com" phone="555-555-5555"/>    
-    </div>
+      <Header branding="Conatct Manager" />
+      <div className="container">
+              <Contact name="john D" email="jhonDoe@gmail.com" phone="555-555-5555" />
+              <Contact name="abc" email="abc@gmail.com" phone="555-555-5555"/>    
+      </div>
+
+      </div>
   );
 }
 
