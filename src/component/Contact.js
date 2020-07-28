cf153f80b0d39d678402408f73748bb84f69ccac
chetin4ever <hetan009@gmail.com>
2020-07-28 17:38:03 +0530

    added reducer for action

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';//impt
+import { Consumer } from '../context';
 // import './contact.css';
 
 class Contact extends Component {
@@ -12,33 +13,42 @@ class Contact extends Component {
             showContactInfo: !this.state.showContactInfo
         })
     }
-    onDeleteClick = (e) => {
+    onDeleteClick = (id,dispatch) => {
         //console.log("click on delete button");
-        this.props.deleteClickHandler();
+        dispatch({type:'DELETE_CONTACT',payload:id})
+        
     }
     render() {
-        const { name, email, phone } = this.props.contact;
+        const { id,name, email, phone } = this.props.contact;
         const { showContactInfo } = this.state;
-        console.log(showContactInfo);
+        //console.log(showContactInfo);
         return (
+            <Consumer>
+                {value => {
+                    const { dispatch } = value;
+                    return (
+                        
             <div className="card card-body mb-3">
                 <h4>{name}
                     <i className="fas fa-sort-down" style={{ cursor: 'pointer' }} onClick={this.onShowClick}> </i>
-                    <i className="fa fa-times" style={{ cusrsor: 'pointer', color: 'red', float: 'right' }} onClick={this.onDeleteClick}></i>
+                                <i className="fa fa-times" style={{ cusrsor: 'pointer', color: 'red', float: 'right' }} onClick={()=>this.onDeleteClick(id,dispatch)}></i>
                 </h4>
                 {showContactInfo ? (
                     <ul className="list-group">
                     <li className="list-group-item">Email:{email}</li>
-                    <li className="list-group-item">Phone:{phone}</li>deleteHandler
+                    <li className="list-group-item">Phone:{phone}</li>
                 </ul>
                 ):null}
             </div>
+                    )
+                }}
+            </Consumer>
         )
     }
 }
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
-    deleteClickHandler:PropTypes.func.isRequired,
+   // deleteClickHandler:PropTypes.func.isRequired,
     //phone: PropTypes.bool.isRequired,
     
 }
--- src/component/Contacts.js
+++ src/component/Contacts.js
@@ -1,36 +1,23 @@
 import React, { Component } from "react";
 import Contact from "./Contact";
-import { Consumer}from '../context'
+import { Consumer } from "../context";
 
 class Contacts extends Component {
-  
-    deleteContact = (id) => { 
-        console.log(id);
-        const { contacts } = this.state;
-        const newContact = contacts.filter(contact => {
-            return contact.id!==id
-        })
-        this.setState({
-            contacts: newContact
-        })
-    }
   render() {
-    
     return (
       <Consumer>
-        {value => {
+        {(value) => {
           const { contacts } = value;
           return (
             <React.Fragment>
-        {contacts.map((contact) => {
-          //return (<Contact contact={contact} key={contact.id} />)
-            return <Contact contact={contact} key={contact.id} deleteClickHandler={()=>this.deleteContact(contact.id)}/>;
-        })}
-      </React.Fragment>
-          )
-      }}
-     </Consumer>
-     
+              {contacts.map((contact) => {
+                //return (<Contact contact={contact} key={contact.id} />)
+                return <Contact contact={contact} key={contact.id} />;
+              })}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
     );
   }
 }
