cfac52c901bb2486b77f52f4511c5b733ea3b3bc
chetin4ever <hetan009@gmail.com>
2020-07-30 17:22:15 +0530

    add error checking to contact form and i calssnames for adding bootstrap classes

--- src/component/contacts/AddContact.js
+++ src/component/contacts/AddContact.js
@@ -1,18 +1,33 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
 import { v1 as uuid } from "uuid";
+import TextInputGroup from "../layout/TextInputGroup";
 
 class AddContact extends Component {
   state = {
     name: "",
     email: "",
     phone: "",
+    errors: "",
   };
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     console.log(this.state);
+    //error check
     const { name, email, phone } = this.state;
+    if (name === "") {
+      this.setState({ errors: { name: "Name is required" } });
+      return;
+    }
+    if (email === "") {
+      this.setState({ errors: { email: "Email is required" } });
+      return;
+    }
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is required" } });
+      return;
+    }
     const newContact = {
       id: uuid(),
       name,
@@ -25,10 +40,12 @@ class AddContact extends Component {
       name: "",
       email: "",
       phone: "",
+      errors: {},
     });
   };
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
+
     return (
       <Consumer>
         {(value) => {
@@ -38,39 +55,31 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      value={name}
-                      className="form-control form-control-lg"
-                      placeholder="Enter name..."
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      value={email}
-                      className="form-control form-control-lg"
-                      placeholder="Enter email..."
-                      onChange={this.onChange}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="text"
-                      name="phone"
-                      value={phone}
-                      className="form-control form-control-lg"
-                      placeholder="Enter phone..."
-                      onChange={this.onChange}
-                    />
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter Name"
+                    value={name}
+                    onChange={this.onChange}
+                    error={errors.name}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    placeholder="Enter Email"
+                    type="email"
+                    value={email}
+                    onChange={this.onChange}
+                    error={errors.email}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    placeholder="Enter phone"
+                    value={phone}
+                    onChange={this.onChange}
+                    error={errors.phone}
+                  />
                   <input
                     type="submit"
                     value="Add Contact"
