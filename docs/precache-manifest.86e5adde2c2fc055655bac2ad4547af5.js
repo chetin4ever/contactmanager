5f247c78e33aaf1f807548fd4dc4384d6b6a5e42
chetin4ever <hetan009@gmail.com>
2020-07-31 20:28:16 +0530

    changing route path for github pages

---
+++ docs/precache-manifest.86e5adde2c2fc055655bac2ad4547af5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3930b9327a07a8da713fa6047b5d18b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "deba5f51e607c5c8fdb0",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "16df8aafaefb6fc8b2f7",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "deba5f51e607c5c8fdb0",
+    "url": "/contactmanager/static/js/2.c13f4748.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.c13f4748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16df8aafaefb6fc8b2f7",
+    "url": "/contactmanager/static/js/main.c82cf586.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
